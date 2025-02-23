e353bc1ecec41149434a624b49884a5629576f11
romanstr-code <romanstrogoteanu1990@gmail.com>
2025-02-23 19:47:58 +0000

    Added 'Simple test' that goes to the page, checks the text on the page, then adds an element and clicks the click

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,15 @@
 const { expect, test } = require('@playwright/test');
 
-test('First test', async ({ page }) =>{
-    // Write testing code here
-    await page.goto('https://playwright.dev/');
-    await page.pause();
-})
+//test('First test', async ({ page }) =>{
+//   // Write testing code here
+//    await page.goto('https://playwright.dev/');
+//    await page.pause();
+//});
+
+test('Sipmet test', async ({ page }) =>{
+    await page.goto('https://the-internet.herokuapp.com/'); // Go to the website
+    const element = page.locator('text=Add/Remove Elements'); // Find the element
+    const addElement = page.locator('text=Add Element'); // Find the element
+    await element.click(); // Click the element
+    await addElement.click(); // Click the element
+});
