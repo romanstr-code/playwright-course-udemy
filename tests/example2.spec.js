0398c83761f5d1a818c0b1428a772f95194fb8e0
romanstr-code <romanstrogoteanu1990@gmail.com>
2025-02-28 19:52:39 +0000

    Added initial Playwright test cases for site navigation and interaction. Included tests for Playwright site verification and basic element interactions on Herokuapp.

---
+++ tests/example2.spec.js
@@ -0,0 +1,20 @@
+const { test, expect } = require('@playwright/test');
+
+test('First test', async ({ page }) =>{
+    // write testing code here
+    await page.goto("https://playwright.dev");
+    const title = page.locator('.navbar__inner .navbar__title');
+    await expect(title).toHaveText('Playwright');
+});
+
+test('Simple test click test', async ({ page }) => {
+    await page.goto("https://the-internet.herokuapp.com");
+    await page.click("text=Add/Remove Elements");
+    await page.click("text=Add Element");
+});
+
+test('Duplicate test', async ({ page })=> {
+    await page.goto("https://the-internet.herokuapp.com");
+    await page.click("text=Add/Remove Elements");
+    await page.click("text=Add Element");
+})
