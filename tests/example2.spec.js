f9ba67785d7cf2ac7127fa92ae94464758fadaab
romanstr-code <romanstrogoteanu1990@gmail.com>
2025-03-03 11:57:54 +0000

    Tagging tests

--- tests/example2.spec.js
+++ tests/example2.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@playwright/test');
 
-test('First test', async ({ page }) =>{
+test('First test @smoke', async ({ page }) =>{
     // write testing code here
     await page.goto("https://playwright.dev");
     const title = page.locator('.navbar__inner .navbar__title');
@@ -8,15 +8,21 @@ test('First test', async ({ page }) =>{
 });
 
 // Skips the test
-test.skip('Simple test click test', async ({ page, browserName }) => {
+test('Simple test click test @regression', async ({ page, browserName }) => {
     test.skip(browserName= 'firefox', 'Working on the firefox fix');
     await page.goto("https://the-internet.herokuapp.com");
     await page.click("text=Add/Remove Elements");
     await page.click("text=Add Element");
 });
 
-test('Duplicate test', async ({ page })=> {
+test('Duplicate test 1 @smoke', async ({ page })=> {
     await page.goto("https://the-internet.herokuapp.com");
     await page.click("text=Add/Remove Elements");
     await page.click("text=Add Element");
 })
+
+test('Duplicate test 1 @regression', async ({ page })=> {
+    await page.goto("https://the-internet.herokuapp.com");
+    await page.click("text=Add/Remove Elements");
+    await page.click("text=Add Element");
+});
