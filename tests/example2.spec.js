4dfce5f3defa99f584399b529e9f5993a644188b
romanstr-code <romanstrogoteanu1990@gmail.com>
2025-03-04 13:11:53 +0000

    Added 'beforeEach' and 'afterEach' hooks

--- tests/example2.spec.js
+++ tests/example2.spec.js
@@ -3,24 +3,28 @@ const { test, expect } = require('@playwright/test');
 // Grouping the test
 test.describe("Smoke Tests", () => {
 
+    // beforeEach Hook - travels to the specified page first
+    test.beforeEach(async ({page})=>{
+        await page.goto("/");
+    });
+
+    // afterEach hook
+    test.afterEach(async ({page})=>{
+        console.log("This test finished.")
+    })
+
     // Skips the test
     test('Simple test click test @regression', async ({ page, browserName }) => {
-        await page.goto("/");
         await page.click("text=Add/Remove Elements");
-        const locator1 = page.locator("text=Add/Remove Elements");
-        // screenshot targets only the locator specified.
-        await locator1.screenshot({path:'screenshot2.png'});
         await page.click("text=Add Element");
     });
 
     test('Duplicate test 1 @smoke', async ({ page }) => {
-        await page.goto("/");
         await page.click("text=Add/Remove Elements");
         await page.click("text=Add Element");
     })
 
     test('Duplicate test 1 @regression', async ({ page }) => {
-        await page.goto("/");
         await page.click("text=Add/Remove Elements");
         await page.click("text=Add Element");
     });
